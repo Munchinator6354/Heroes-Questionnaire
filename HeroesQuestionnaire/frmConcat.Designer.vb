dca8052fe79bb260f19e6b21c96608cb0533a6aa
Ryan Isaacson <munchinator6354@gmail.com>
2020-10-03 17:57:25 -0700

    Add project files.

---
+++ HeroesQuestionnaire/frmConcat.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmConcat
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.lblQuestion1 = New System.Windows.Forms.Label()
+        Me.lblQuestion2 = New System.Windows.Forms.Label()
+        Me.txtAnswer1 = New System.Windows.Forms.TextBox()
+        Me.lblQuestion3 = New System.Windows.Forms.Label()
+        Me.btnStartQuestionnaire = New System.Windows.Forms.Button()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExitProgram = New System.Windows.Forms.Button()
+        Me.txtAnswer2 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer3 = New System.Windows.Forms.TextBox()
+        Me.lblStoryOutput = New System.Windows.Forms.Label()
+        Me.lnkMontyPythonVid = New System.Windows.Forms.LinkLabel()
+        Me.picAskMeTheQuestions = New System.Windows.Forms.PictureBox()
+        CType(Me.picAskMeTheQuestions, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Location = New System.Drawing.Point(4, 4)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(218, 51)
+        Me.lblID.TabIndex = 0
+        Me.lblID.Text = "Ryan Isaacson" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "CSS 115 Fall 2020" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Exercise 3 - String Concatenation"
+        '
+        'lblQuestion1
+        '
+        Me.lblQuestion1.AutoSize = True
+        Me.lblQuestion1.Location = New System.Drawing.Point(364, 108)
+        Me.lblQuestion1.Name = "lblQuestion1"
+        Me.lblQuestion1.Size = New System.Drawing.Size(0, 17)
+        Me.lblQuestion1.TabIndex = 2
+        Me.lblQuestion1.Visible = False
+        '
+        'lblQuestion2
+        '
+        Me.lblQuestion2.AutoSize = True
+        Me.lblQuestion2.Location = New System.Drawing.Point(364, 170)
+        Me.lblQuestion2.Name = "lblQuestion2"
+        Me.lblQuestion2.Size = New System.Drawing.Size(51, 17)
+        Me.lblQuestion2.TabIndex = 3
+        Me.lblQuestion2.Text = "Label2"
+        Me.lblQuestion2.Visible = False
+        '
+        'txtAnswer1
+        '
+        Me.txtAnswer1.Location = New System.Drawing.Point(576, 108)
+        Me.txtAnswer1.Name = "txtAnswer1"
+        Me.txtAnswer1.Size = New System.Drawing.Size(169, 22)
+        Me.txtAnswer1.TabIndex = 5
+        Me.txtAnswer1.Visible = False
+        '
+        'lblQuestion3
+        '
+        Me.lblQuestion3.AutoSize = True
+        Me.lblQuestion3.Location = New System.Drawing.Point(364, 233)
+        Me.lblQuestion3.Name = "lblQuestion3"
+        Me.lblQuestion3.Size = New System.Drawing.Size(51, 17)
+        Me.lblQuestion3.TabIndex = 4
+        Me.lblQuestion3.Text = "Label3"
+        Me.lblQuestion3.Visible = False
+        '
+        'btnStartQuestionnaire
+        '
+        Me.btnStartQuestionnaire.Location = New System.Drawing.Point(299, 134)
+        Me.btnStartQuestionnaire.Name = "btnStartQuestionnaire"
+        Me.btnStartQuestionnaire.Size = New System.Drawing.Size(195, 116)
+        Me.btnStartQuestionnaire.TabIndex = 1
+        Me.btnStartQuestionnaire.Text = "Start the " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Heroes Questionnaire!" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Click Me" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "To Begin"
+        Me.btnStartQuestionnaire.UseVisualStyleBackColor = True
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(45, 343)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(201, 76)
+        Me.btnRun.TabIndex = 10
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        Me.btnRun.Visible = False
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(295, 343)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(199, 76)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        Me.btnClear.Visible = False
+        '
+        'btnExitProgram
+        '
+        Me.btnExitProgram.Location = New System.Drawing.Point(546, 343)
+        Me.btnExitProgram.Name = "btnExitProgram"
+        Me.btnExitProgram.Size = New System.Drawing.Size(199, 76)
+        Me.btnExitProgram.TabIndex = 12
+        Me.btnExitProgram.Text = "Exit"
+        Me.btnExitProgram.UseVisualStyleBackColor = True
+        Me.btnExitProgram.Visible = False
+        '
+        'txtAnswer2
+        '
+        Me.txtAnswer2.Location = New System.Drawing.Point(576, 170)
+        Me.txtAnswer2.Name = "txtAnswer2"
+        Me.txtAnswer2.Size = New System.Drawing.Size(169, 22)
+        Me.txtAnswer2.TabIndex = 6
+        Me.txtAnswer2.Visible = False
+        '
+        'txtAnswer3
+        '
+        Me.txtAnswer3.Location = New System.Drawing.Point(576, 233)
+        Me.txtAnswer3.Name = "txtAnswer3"
+        Me.txtAnswer3.Size = New System.Drawing.Size(169, 22)
+        Me.txtAnswer3.TabIndex = 7
+        Me.txtAnswer3.Visible = False
+        '
+        'lblStoryOutput
+        '
+        Me.lblStoryOutput.AutoSize = True
+        Me.lblStoryOutput.Location = New System.Drawing.Point(100, 104)
+        Me.lblStoryOutput.Name = "lblStoryOutput"
+        Me.lblStoryOutput.Size = New System.Drawing.Size(123, 17)
+        Me.lblStoryOutput.TabIndex = 9
+        Me.lblStoryOutput.Text = "Story Output Here"
+        Me.lblStoryOutput.Visible = False
+        '
+        'lnkMontyPythonVid
+        '
+        Me.lnkMontyPythonVid.AutoSize = True
+        Me.lnkMontyPythonVid.Location = New System.Drawing.Point(139, 309)
+        Me.lnkMontyPythonVid.Name = "lnkMontyPythonVid"
+        Me.lnkMontyPythonVid.Size = New System.Drawing.Size(545, 17)
+        Me.lnkMontyPythonVid.TabIndex = 8
+        Me.lnkMontyPythonVid.TabStop = True
+        Me.lnkMontyPythonVid.Text = "This Questionnaire is inspired by this Monty Python and the Holy Grail snippet, E" &
+    "njoy!"
+        '
+        'picAskMeTheQuestions
+        '
+        Me.picAskMeTheQuestions.Location = New System.Drawing.Point(25, 75)
+        Me.picAskMeTheQuestions.Name = "picAskMeTheQuestions"
+        Me.picAskMeTheQuestions.Size = New System.Drawing.Size(333, 231)
+        Me.picAskMeTheQuestions.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picAskMeTheQuestions.TabIndex = 13
+        Me.picAskMeTheQuestions.TabStop = False
+        Me.picAskMeTheQuestions.Visible = False
+        '
+        'frmConcat
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.picAskMeTheQuestions)
+        Me.Controls.Add(Me.lnkMontyPythonVid)
+        Me.Controls.Add(Me.lblStoryOutput)
+        Me.Controls.Add(Me.txtAnswer3)
+        Me.Controls.Add(Me.txtAnswer2)
+        Me.Controls.Add(Me.btnExitProgram)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.btnStartQuestionnaire)
+        Me.Controls.Add(Me.lblQuestion3)
+        Me.Controls.Add(Me.txtAnswer1)
+        Me.Controls.Add(Me.lblQuestion2)
+        Me.Controls.Add(Me.lblQuestion1)
+        Me.Controls.Add(Me.lblID)
+        Me.Name = "frmConcat"
+        Me.Text = "Heroes Questionnaire Concatenation Form"
+        CType(Me.picAskMeTheQuestions, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblID As Label
+    Friend WithEvents lblQuestion1 As Label
+    Friend WithEvents lblQuestion2 As Label
+    Friend WithEvents txtAnswer1 As TextBox
+    Friend WithEvents lblQuestion3 As Label
+    Friend WithEvents btnStartQuestionnaire As Button
+    Friend WithEvents btnRun As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExitProgram As Button
+    Friend WithEvents txtAnswer2 As TextBox
+    Friend WithEvents txtAnswer3 As TextBox
+    Friend WithEvents lblStoryOutput As Label
+    Friend WithEvents lnkMontyPythonVid As LinkLabel
+    Friend WithEvents picAskMeTheQuestions As PictureBox
+End Class
---
+++ HeroesQuestionnaire/frmConcat.vb
@@ -0,0 +1,161 @@
+﻿Option Strict On
+Option Explicit On
+'Instructor: Penny Russell 
+'Class: CS115 Fall 2020
+'Student: Ryan Isaacson
+'Assignment: Exercise 3 String Concatenation
+
+Public Class frmConcat
+    'This Subroutine handles Clicking the Start Questionnaire button and inializes the program
+    Private Sub btnStartQuestionnaire_Click(sender As Object, e As EventArgs) Handles btnStartQuestionnaire.Click
+        lblQuestion1.Text = "1) What is your name?"
+        lblQuestion2.Text = "2) What do you quest for?"
+        lblQuestion3.Text = "3) What is your favorite color?"
+        lblQuestion1.Show()
+        txtAnswer1.Show()
+        btnRun.Text = "Click for" & vbCrLf & "Next Question"
+        btnRun.Show()
+        btnClear.Text = "Clear" & vbCrLf & "Answers"
+        btnClear.Show()
+        btnExitProgram.Text = "Exit" & vbCrLf & "Program"
+        btnExitProgram.Show()
+        btnStartQuestionnaire.Hide()
+        picAskMeTheQuestions.Image = Image.FromFile("..\Images\AskMeTheQuestions.jpg")
+        picAskMeTheQuestions.Show()
+        txtAnswer1.Focus()
+    End Sub
+
+    'This subroutine handles the "enter key" submission of answer 1
+    Private Sub txtAnswer1_KeyDown(sender As Object, e As KeyEventArgs) Handles txtAnswer1.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If txtAnswer1.Text = "" Then
+                MessageBox.Show("Hero! You must enter your name!")
+            Else
+                lblQuestion2.Show()
+                txtAnswer2.Show()
+                txtAnswer2.Focus()
+                e.SuppressKeyPress = True
+            End If
+        End If
+    End Sub
+
+    'This subroutine handles the "enter key" submission of answer 2
+    Private Sub txtAnswer2_KeyDown(sender As Object, e As KeyEventArgs) Handles txtAnswer2.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If txtAnswer2.Text = "" Then
+                MessageBox.Show("Hero! You must enter what it is you quest for!")
+            Else
+                lblQuestion3.Show()
+                txtAnswer3.Show()
+                btnRun.Text = "Finish Heroes" & vbCrLf & "Questionnaire"
+                txtAnswer3.Focus()
+                e.SuppressKeyPress = True
+            End If
+        End If
+    End Sub
+
+    'This subroutine handles the "enter key" submission of answer 3
+    Private Sub txtAnswer3_KeyDown(sender As Object, e As KeyEventArgs) Handles txtAnswer3.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If txtAnswer3.Text = "" Then
+                MessageBox.Show("Hero! Everyone has a favorite color, come on now!")
+            Else
+                Dim name As String
+                Dim quest As String
+                Dim color As String
+                Dim story As String
+                name = txtAnswer1.Text
+                quest = txtAnswer2.Text
+                color = txtAnswer3.Text
+                story = "Our heroes' name is " & name & "." & vbCrLf & vbCrLf &
+                        "They quest for " & quest & "." & vbCrLf & vbCrLf &
+                        "But most importantly, their favorite color is " & color & "!"
+                lblStoryOutput.Text = story
+                picAskMeTheQuestions.Hide()
+                btnClear.Focus()
+                e.SuppressKeyPress = True
+                lblQuestion1.Hide()
+                lblQuestion2.Hide()
+                lblQuestion3.Hide()
+                txtAnswer1.Hide()
+                txtAnswer2.Hide()
+                txtAnswer3.Hide()
+                lblStoryOutput.Show()
+                btnClear.Text = "Restart" & vbCrLf & "Questionnaire?"
+                btnRun.Hide()
+            End If
+        End If
+    End Sub
+
+    'This subroutine handles the "mouse click" submission of the questionnaire progression button
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        If txtAnswer1.Text = "" Then
+            MessageBox.Show("Hero! You must enter your name!")
+        ElseIf lblQuestion2.Visible = False Then
+            lblQuestion2.Show()
+            txtAnswer2.Show()
+            txtAnswer2.Focus()
+        ElseIf txtAnswer2.Text = "" Then
+            MessageBox.Show("Hero! You must enter what it is you quest for!")
+        ElseIf lblQuestion3.Visible = False Then
+            btnRun.Text = "Finish Heroes" & vbCrLf & "Questionnaire"
+            lblQuestion3.Show()
+            txtAnswer3.Show()
+            txtAnswer3.Focus()
+        ElseIf txtAnswer3.Text = "" Then
+            MessageBox.Show("Hero! Everyone has a favorite color, come on now!")
+        Else
+            Dim name As String
+            Dim quest As String
+            Dim color As String
+            Dim story As String
+            name = txtAnswer1.Text
+            quest = txtAnswer2.Text
+            color = txtAnswer3.Text
+            story = "Our heroes' name is " & name & "." & vbCrLf & vbCrLf &
+                        "They quest for " & quest & "." & vbCrLf & vbCrLf &
+                        "But most importantly, their favorite color is " & color & "!"
+            lblStoryOutput.Text = story
+            picAskMeTheQuestions.Hide()
+            btnClear.Focus()
+            lblQuestion1.Hide()
+            lblQuestion2.Hide()
+            lblQuestion3.Hide()
+            txtAnswer1.Hide()
+            txtAnswer2.Hide()
+            txtAnswer3.Hide()
+            lblStoryOutput.Show()
+            btnClear.Text = "Restart" & vbCrLf & "Questionnaire?"
+            btnRun.Hide()
+        End If
+    End Sub
+
+    'This subroutine handles the Clear Answers button
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblQuestion2.Hide()
+        lblQuestion3.Hide()
+        txtAnswer2.Hide()
+        txtAnswer3.Hide()
+        txtAnswer1.Text = ""
+        txtAnswer2.Text = ""
+        txtAnswer3.Text = ""
+        lblStoryOutput.Text = ""
+        lblQuestion1.Show()
+        txtAnswer1.Show()
+        btnRun.Show()
+        txtAnswer1.Focus()
+        picAskMeTheQuestions.Show()
+        btnRun.Text = "Click for" & vbCrLf & "Next Question"
+    End Sub
+
+    'This subroutine opens up the video that references the underlying theme of the app
+    Private Sub lnkMontyPythonVid_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkMontyPythonVid.LinkClicked
+        Process.Start("https://www.youtube.com/watch?v=pWS8Mg-JWSg")
+    End Sub
+
+    'This subroutine handles the Exit Program button
+    Private Sub btnExitProgram_Click(sender As Object, e As EventArgs) Handles btnExitProgram.Click
+        Me.Close()
+    End Sub
+
+End Class
